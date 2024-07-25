a21d895ff1b39286d565763d15752c54daab3625
jk2403 <jayakumar586@gmail@gmail.com>
2024-07-25 12:04:10 +0530

    Pushing my new Project code

---
+++ src/main/java/one/SeleniumTestDemo1.java
@@ -0,0 +1,27 @@
+package one;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTestDemo1 {
+
+	@Test
+	public static void testNGDemo() {
+
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in");
+		System.out.println("======Test Initiated Successfully==========");
+		
+		System.out.println("============="+driver.getTitle()+"=========");
+		
+		driver.quit();
+		System.out.println("======Test Completed Successfully==========");
+	}
+
+}
